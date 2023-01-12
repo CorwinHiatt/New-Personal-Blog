ba4bdcef109ab30df9420be450c95ad1ed76e344
CorwinHiatt <corwinhiatt@yahoo.com>
2023-01-11 23:25:09 -0600

    padding those squares with smaller commits consider I'm working on someone else project on my own personal branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import { Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+     
    
       </header>
     </div>
